bd4a241acaa8f77d8271fd2c7313971c9e025111
Stone Zhong <stonezhong@hotmail.com>
2019-02-26 03:30:17 -0800

    update

--- python/HelloWorld/src/main/demo_003.py
+++ python/HelloWorld/src/main/demo_003.py
@@ -2,17 +2,12 @@ from __future__ import print_function
 
 import sys
 from pyspark.sql import SparkSession, SQLContext, Row
-from pyspark.sql.types import StructType, StructField, LongType, StringType
-import datetime
+from pyspark.sql.types import StructType, StructField, LongType, StringType, DateType
+from datetime import datetime, date
 import tempfile
 
 # this demo shows how to create a parquet with partition
 # also read https://spark.apache.org/docs/latest/sql-data-sources-parquet.html
-def epoh_time(year, month, day):
-    epoch = datetime.datetime.utcfromtimestamp(0)
-    dt = datetime.datetime(year = year, month=month, day = day)
-    return int((dt - epoch).total_seconds())
-
 def main(spark):
     sc = spark.sparkContext
     sql_context = SQLContext(sc)
@@ -21,34 +16,35 @@ def main(spark):
     print("base_dir = {}".format(base_dir))
 
     schema = StructType([
-        StructField("id",     LongType(),   True),
-        StructField("name",   StringType(), True),
+        StructField("id",      LongType(),   True),
+        StructField("gender",  StringType(), True),
+        StructField("_time",   DateType(),   True),
+        StructField("name",    StringType(), True),
     ])
 
-    # write to partition 1
-    _time=epoh_time(2019, 1, 1)
-    rdd = sc.parallelize([
-        Row(id=1, name="foo"), 
-        Row(id=2, name="bar"),
-    ], 1)
-    df = sql_context.createDataFrame(rdd, schema)
-    df.write.mode('append').parquet("file://{}/_time={}/students.parquet".format(base_dir, _time))
+    Person = Row("id", "gender", "_time", "name")
 
-    # write to partition 2
-    _time=epoh_time(2019, 1, 2)
     rdd = sc.parallelize([
-        Row(id=1, name="tar"), 
+        Person(1, "male",   date(2019, 1, 1), "James Bond"),
+        Person(2, "female", date(2019, 1, 1), "Emma Smith"),
+        Person(3, "male",   date(2019, 1, 2), "Anakin Skywalker"),
     ], 1)
     df = sql_context.createDataFrame(rdd, schema)
-    df.write.mode('append').parquet("file://{}/_time={}/students.parquet".format(base_dir, _time))
+    df\
+        .write\
+        .mode('append')\
+        .partitionBy(["gender", "_time"])\
+        .parquet("file://{}/students.parquet".format(base_dir))
 
     print("==================")
     print("base_dir: {}".format(base_dir))
     print("==================")
 
     # Now let's load parquet
-    df = sql_context.read.parquet("file://{}/".format(base_dir))
-    df.filter(df._time == epoh_time(2019, 1, 1)).show()
+    df = sql_context.read.parquet("file://{}/students.parquet".format(base_dir))
+    df.printSchema()
+    df.show()
+    df.filter(df._time == date(2019, 1, 1)).show()
 
 def run():    
     spark = SparkSession\
